257ff6fc76c88373dc321ec0516e05e68b12638c
Noah Mezher <noahmezher@gmail.com>
2018-07-27 15:12:34 -0400

    updated etch-a-sketch

--- EtchASketch/EtchASketch.js
+++ EtchASketch/EtchASketch.js
@@ -1,16 +1,63 @@
-function generate_divs() {
-    var e = document.body;
-    for(var i = 0; i < 16; i++) {
-        var row = document.createElement("div");
-        row.className = "row";
-        for(var j = 1; j <= 16; j++) {
+const height_width = 512;
+var num_cells = 16;
+var mode = "default";
+
+
+function generate_divs(cells) {
+    var e = document.getElementById("etchASketchContainer");
+    var cell_dimension = height_width / cells;
+
+    for(var i = 0; i < cells; i++) {
+        var column = document.createElement("div");
+        column.className = "column";
+        for(var j = 1; j <= cells; j++) {
             var cell = document.createElement("div");
+
             cell.className = "grid_element";
-            cell.innerText = "" + ((i * 16) + j);
-            row.appendChild(cell);
+            cell.style.width = (cell_dimension + 'px');
+            cell.style.height = (cell_dimension + 'px');
+            cell.addEventListener("mouseover", change_color);
+            column.appendChild(cell);
         }
-        e.appendChild(row);
+        e.appendChild(column);
     }
-    //document.getElementById("code").innerText = e.innerHTML;
 }
-generate_divs();
+function clear_divs() {
+    var e = document.getElementById("etchASketchContainer");
+    while(e.firstChild) {
+        e.removeChild(e.firstChild);
+    }
+}
+function change_color() {
+    var single_color = document.getElementById("singleColor");
+    var random_color = document.getElementById("randomColor");
+    var shaded_color = document.getElementById("shadedColor");
+    if(single_color.checked)
+        this.style.backgroundColor = "red";
+    else if(random_color.checked) {
+        var color = generate_random_color();
+        this.style.backgroundColor = color;
+    }
+    else if(shaded_color.checked)
+        this.style.backgroundColor = "black";
+}
+function shake() {
+    var cells = prompt("Enter the number of cells you want (a number between 2 and 100)");
+    if(cells == null)
+        cells = 16;
+    while(cells < 2 || cells > 100) {
+        cells = prompt("Please enter a number between 2 and 100");
+    }
+    clear_divs();
+    generate_divs(cells);
+}
+function generate_random_color() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+
+    for(var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+generate_divs(num_cells);
